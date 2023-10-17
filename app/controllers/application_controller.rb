6d7c8daa9d690acfb7b18eb2236fe8ac3a9c4629
kamran ul haq <kamranulhaq2002@gmail.com>
2023-10-17 13:12:59 +0500

    p7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotDestroyed,with: :not_destroyed
+
+  private
+  def not_destroyed(e)
+      render json: {errors: e.record.errors }, status: :unprocessable_entity
+  end
 end
