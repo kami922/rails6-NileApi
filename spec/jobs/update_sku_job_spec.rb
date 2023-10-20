57f94dcf630bbed478052df10272b7bc732aeefc
kamran ul haq <kamranulhaq2002@gmail.com>
2023-10-20 13:09:57 +0500

    p17

--- spec/jobs/update_sku_job_spec.rb
+++ spec/jobs/update_sku_job_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe UpdateSkuJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:book_name) {'eleoquent ruby'}
+  it 'calls SKU service with correct params' do
+    allow(Net::HTTP).to receive(:start).and_return(true)
+    expect_any_instance_of(Net::HTTP::Post).to receive(:body=).with(
+      {sku: '123', name: book_name}.to_json
+    )
+    described_class.perform_now(book_name)
+  end
 end
