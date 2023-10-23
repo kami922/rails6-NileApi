4f1f2ae207a166c009048cd55eb69f44e6a8b7f6
kamran ul haq <kamranulhaq2002@gmail.com>
2023-10-23 15:51:11 +0500

    p21

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,9 +4,10 @@ module Api
     rescue_from ActionController::ParameterMissing, with: :param_missing
       def create
         # puts params.inspect
-        p params.require(:username).inspect
         p params.require(:password).inspect
-        render json:{ token:'123'}, status: :created
+        user = User.find_by(username: params.require(:username))
+        token = AuthenticationTokenService.call(user.id)
+        render json:{ token:token}, status: :created
       end
 
       private
@@ -17,3 +18,5 @@ module Api
     end
   end
 end
+
+# curl -X POST http://localhost:3000/api/v1/authenticate -H "Content-Type: application/json" -d '{"username": "BookSeller69","password":"blah"}'
