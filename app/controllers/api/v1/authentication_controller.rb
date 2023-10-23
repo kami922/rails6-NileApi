0699c1ae73d7f6bce3626488b5494be215bf83da
kamran ul haq <kamranulhaq2002@gmail.com>
2023-10-23 17:24:16 +0500

    p22

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,11 +1,14 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError;end
     rescue_from ActionController::ParameterMissing, with: :param_missing
+    rescue_from AuthenticationError, with: :handle_unauthenticated
       def create
         # puts params.inspect
         p params.require(:password).inspect
         user = User.find_by(username: params.require(:username))
+        raise AuthenticationError unless user.authenticate(params.require(:password))
         token = AuthenticationTokenService.call(user.id)
         render json:{ token:token}, status: :created
       end
@@ -13,6 +16,10 @@ module Api
       private
       def param_missing(e)
         render json: { error: e.message },status: :unprocessable_entity
+      end
+
+      def handle_unauthenticated
+        head :unauthorized
       end
 
     end
